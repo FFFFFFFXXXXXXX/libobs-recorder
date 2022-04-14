4baced930c62a34130320bfdc459c613e76988e0
FX <felix.arbeithuber@gmail.com>
2022-04-14 15:42:25 +0200

    small fixes

--- examples/record.rs
+++ examples/record.rs
@@ -41,6 +41,7 @@ fn main() {
     settings.set_input_resolution(libobs_recorder::resolution::Resolution::_1080p);
     settings.set_output_resolution(libobs_recorder::resolution::Resolution::_1440p);
     settings.set_framerate(Framerate::new(45));
+    settings.record_audio(false);
     recorder.configure(settings).unwrap();
 
     std::thread::sleep(std::time::Duration::from_secs(5));
@@ -61,7 +62,7 @@ fn main() {
     settings.set_output_path("C:/Users/Felix/Videos/Test3.mp4".to_string());
     settings.set_framerate(Framerate::new(60));
     settings.set_cbr(Cbr::mbit(20));
-    settings.record_audio(false);
+    settings.record_audio(true);
     recorder.configure(settings).unwrap();
 
     std::thread::sleep(std::time::Duration::from_secs(5));

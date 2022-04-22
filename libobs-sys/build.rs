09b697ca58bca95203cd24dd2cee762cfb96b0ec
FX <felix.arbeithuber@gmail.com>
2022-04-23 01:28:52 +0200

    redo stuff

--- libobs-sys/build.rs
+++ libobs-sys/build.rs
@@ -1,6 +1,11 @@
 extern crate bindgen;
 
 fn main() {
+    // require linking obs.dll
+    // specify folder in your project build.rs with:
+    // println!("cargo:rustc-link-search=native=./my/folder");
+    println!("cargo:rustc-link-lib=obs");
+
     let bindings = bindgen::builder()
         .header("libobs_headers/obs.h")
         .blacklist_type("_bindgen_ty_2")

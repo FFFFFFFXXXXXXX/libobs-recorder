3ed125085ebd35097e329e2f5d24f3c32caad4bd
FX <felix.arbeithuber@gmail.com>
2022-05-27 00:21:51 +0200

    test

--- libobs-sys/build.rs
+++ libobs-sys/build.rs
@@ -1,7 +1,11 @@
 extern crate bindgen;
 
 fn main() {
-    println!("cargo:rustc-link-search=native=./libobs-sys/");
+    // println!("cargo:rustc-link-search=native=./libobs-sys/");
+    println!(
+        "cargo:rustc-link-search=native={}",
+        std::env::var("CARGO_MANIFEST_DIR").unwrap()
+    );
     println!("cargo:rustc-link-lib=obs");
 
     let bindings = bindgen::builder()

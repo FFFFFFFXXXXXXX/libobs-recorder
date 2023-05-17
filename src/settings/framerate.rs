9a4da51eaef99b4e033b294471209ad9bbfcf1fb
FX <37913466+FFFFFFFXXXXXXX@users.noreply.github.com>
2023-05-18 00:36:42 +0200

    add serde feature

--- src/settings/framerate.rs
+++ src/settings/framerate.rs
@@ -1,3 +1,4 @@
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Framerate(u32, u32);
 
--- src/settings/window.rs
+++ src/settings/window.rs
@@ -1,3 +1,4 @@
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Clone, PartialEq)]
 pub struct Window {
     name: String,
@@ -27,5 +28,67 @@ impl Window {
             window_id.push_str(process);
         }
         window_id
+    }
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Resolution {
+    _480p,
+    _720p,
+    _1080p,
+    _1440p,
+    _2160p,
+    _4320p,
+}
+
+#[cfg(feature = "full")]
+impl Resolution {
+    pub(crate) fn get_size(&self) -> Size {
+        match self {
+            Resolution::_480p => Size {
+                width: 640,
+                height: 480,
+            },
+            Resolution::_720p => Size {
+                width: 1280,
+                height: 720,
+            },
+            Resolution::_1080p => Size {
+                width: 1920,
+                height: 1080,
+            },
+            Resolution::_1440p => Size {
+                width: 2560,
+                height: 1440,
+            },
+            Resolution::_2160p => Size {
+                width: 3840,
+                height: 2160,
+            },
+            Resolution::_4320p => Size {
+                width: 7680,
+                height: 4320,
+            },
+        }
+    }
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Size {
+    width: u32,
+    height: u32,
+}
+
+impl Size {
+    pub fn new(width: u32, height: u32) -> Self {
+        Size { width, height }
+    }
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+    pub fn height(&self) -> u32 {
+        self.height
     }
 }

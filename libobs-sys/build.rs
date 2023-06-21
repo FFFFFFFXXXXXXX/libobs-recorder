c0d76dcd5708fc8db26622e697e43f24b692c5fc
FX <felix.arbeithuber@gmail.com>
2023-06-21 23:45:07 +0200

    Save bindings and regenerate only if necessary

--- libobs-sys/build.rs
+++ libobs-sys/build.rs
@@ -1,19 +1,28 @@
-extern crate bindgen;
-
 fn main() {
+    // always link libobs
     println!(
         "cargo:rustc-link-search=native={}",
-        std::env::var("CARGO_MANIFEST_DIR").unwrap()
+        env!("CARGO_MANIFEST_DIR")
     );
     println!("cargo:rustc-link-lib=obs");
 
+    // only generate bindings if they don't exist yet
+    let bindings_path =
+        std::path::Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/bindings.rs"));
+    if let Ok(metadata) = bindings_path.metadata() {
+        if metadata.len() > 0 {
+            return;
+        }
+    }
+
     let bindings = bindgen::builder()
         .header("libobs_headers/obs.h")
-        .blacklist_type("_bindgen_ty_1")
-        .blacklist_type("_bindgen_ty_2")
+        .blocklist_type("_bindgen_ty_1")
+        .blocklist_type("_bindgen_ty_2")
         .generate()
         .expect("Error generating bindings");
 
-    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_path.join("bindings.rs")).expect("Error outputting bindings");
+    bindings
+        .write_to_file(bindings_path)
+        .expect("Error outputting bindings");
 }

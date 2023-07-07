6e3b958119f9113536bdcace1a452a27492fa2fb
FX <felix.arbeithuber@gmail.com>
2023-07-07 03:20:27 +0200

    add option to capture microphone input

--- src/settings/audio.rs
+++ src/settings/audio.rs
@@ -1,7 +1,12 @@
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum AudioSource {
+    /// no audio
     NONE,
+    /// only the audio of the window that is being captured
     APPLICATION,
+    /// the default audio output of the pc
     SYSTEM,
+    /// the default audio input and output of the pc
+    ALL,
 }

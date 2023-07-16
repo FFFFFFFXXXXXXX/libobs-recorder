d5b32c6df13fc49254b6cb71139e087488c7a01d
FX <37913466+FFFFFFFXXXXXXX@users.noreply.github.com>
2023-07-16 14:48:12 +0200

    add possibility to run extprocess_recorder in other directories than the current working directory

--- src/lib.rs
+++ src/lib.rs
@@ -8,21 +8,27 @@ pub use intprocess_recorder::{settings::RecorderSettings, InpRecorder as Singlet
 use ipc_link::{IpcCommand, IpcLinkMaster};
 
 #[cfg(target_family = "windows")]
-const EXECUTABLE: &str = "extprocess_recorder.exe";
+const EXECUTABLE: &str = "./extprocess_recorder.exe";
 #[cfg(target_family = "unix")]
-const EXECUTABLE: &str = "extprocess_recorder";
+const EXECUTABLE: &str = "./extprocess_recorder";
 
 pub struct Recorder {
     recorder: IpcLinkMaster,
 }
 
 impl Recorder {
-    pub fn new(
+    pub fn new() -> std::io::Result<Self> {
+        Self::new_with_paths(None, None, None, None)
+    }
+
+    pub fn new_with_paths(
+        executable_path: Option<&std::path::Path>,
         libobs_data_path: Option<&str>,
         plugin_bin_path: Option<&str>,
         plugin_data_path: Option<&str>,
     ) -> std::io::Result<Self> {
-        let mut rec = IpcLinkMaster::new(EXECUTABLE)?;
+        let mut rec =
+            IpcLinkMaster::new(executable_path.unwrap_or(std::path::Path::new(EXECUTABLE)))?;
 
         let cmd = IpcCommand::Init {
             libobs_data_path: libobs_data_path.map(|s| s.to_string()),

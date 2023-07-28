4ca37f23f8415428d12240e647da6c18e821ac1a
FX <37913466+FFFFFFFXXXXXXX@users.noreply.github.com>
2023-07-28 20:49:30 +0200

    Add AV1 HW encoders for NVIDIA, AMD and Intel.
    All AV1 encoders currently have the lowest priority on each platform, which means the library defaults to the normal h264 HW encoder.
    In oder to use the AV1 encoders, they have to be manually set via RecorderSettings.

--- intprocess-recorder/src/settings/encoders.rs
+++ intprocess-recorder/src/settings/encoders.rs
@@ -1,24 +1,38 @@
 use crate::{recorder::obs_data::ObsData, settings::RateControl};
 
-// the encoders are sorted by their default preference
+// the encoders are sorted by their priority
 #[allow(non_camel_case_types)]
 #[derive(serde::Serialize, serde::Deserialize, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Encoder {
+    /// NVIDIA h264 encoder
     JIM_NVENC,
+    /// fallback NVIDIA h264 encoder
     FFMPEG_NVENC,
-    AMD_H264,
-    OBS_QSV11,
+    /// NVIDIA AV1 encoder
+    JIM_AV1,
+    /// AMD h264 encoder
+    AMD_AMF_H264,
+    /// AMD AV1 encoder
+    AMD_AMF_AV1,
+    /// Intel h264 encoder
+    OBS_QSV11_H264,
+    /// Intel AV1 encoder
+    OBS_QSV11_AV1,
+    /// Software h264 encoder
     OBS_X264,
 }
 
 impl Encoder {
     #[must_use]
     pub fn id(&self) -> &str {
-        match *self {
+        match self {
             Self::JIM_NVENC => "jim_nvenc",
             Self::FFMPEG_NVENC => "ffmpeg_nvenc",
-            Self::AMD_H264 => "h264_texture_amf",
-            Self::OBS_QSV11 => "obs_qsv11",
+            Self::JIM_AV1 => "jim_av1_nvenc",
+            Self::AMD_AMF_H264 => "h264_texture_amf",
+            Self::AMD_AMF_AV1 => "av1_texture_amf",
+            Self::OBS_QSV11_H264 => "obs_qsv11_v2",
+            Self::OBS_QSV11_AV1 => "obs_qsv11_av1",
             Self::OBS_X264 => "obs_x264",
         }
     }
@@ -26,9 +40,11 @@ impl Encoder {
     #[must_use]
     pub(crate) fn settings(self, rate_control: RateControl) -> ObsData {
         match self {
-            Self::JIM_NVENC | Self::FFMPEG_NVENC => nvidia_nvenc_settings(rate_control),
-            Self::AMD_H264 => amd_new_h264_settings(rate_control),
-            Self::OBS_QSV11 => intel_quicksync_settings(rate_control),
+            Self::JIM_NVENC | Self::FFMPEG_NVENC => nvidia_h264_settings(rate_control),
+            Self::JIM_AV1 => nvidia_av1_settings(rate_control),
+            Self::AMD_AMF_H264 | Self::AMD_AMF_AV1 => amd_amf_settings(rate_control),
+            Self::OBS_QSV11_H264 => intel_quicksync_h264_settings(rate_control),
+            Self::OBS_QSV11_AV1 => intel_quicksync_av1_settings(rate_control),
             Self::OBS_X264 => obs_x264_settings(rate_control),
         }
     }
@@ -41,45 +57,15 @@ impl TryFrom<&str> for Encoder {
         match value {
             "jim_nvenc" => Ok(Self::JIM_NVENC),
             "ffmpeg_nvenc" => Ok(Self::FFMPEG_NVENC),
-            "h264_texture_amf" => Ok(Self::AMD_H264),
-            "obs_qsv11" => Ok(Self::OBS_QSV11),
+            "h264_texture_amf" => Ok(Self::AMD_AMF_H264),
+            "obs_qsv11" => Ok(Self::OBS_QSV11_H264),
             "obs_x264" => Ok(Self::OBS_X264),
             _ => Err(()),
         }
     }
 }
 
-fn amd_new_h264_settings(rate_control: RateControl) -> ObsData {
-    let mut data = ObsData::new();
-
-    // Picture Control Properties
-    data.set_int("bf", 1);
-    data.set_int("keyint_sec", 2);
-    data.set_string("ffmpeg_opts", "MaxNumRefFrames=4 BReferenceEnable=1 BPicturesPattern=1 MaxConsecutiveBPictures=1 HighMotionQualityBoostEnable=1");
-
-    data.set_string("preset", "quality");
-    data.set_string("profile", "high");
-
-    match rate_control {
-        RateControl::CBR(cbr) => {
-            data.set_string("rate_control", "CBR");
-            data.set_int("bitrate", cbr);
-        }
-        RateControl::VBR(vbr) => {
-            data.set_string("rate_control", "VBR");
-            data.set_int("bitrate", vbr);
-        }
-        RateControl::CQP(cqp) | RateControl::ICQ(cqp) => {
-            let cqp = cqp.clamp(0, 51);
-            data.set_string("rate_control", "CQP");
-            data.set_int("cqp", cqp);
-        }
-        RateControl::CRF(_) => { /* invalid -> do nothing */ }
-    };
-    data
-}
-
-fn nvidia_nvenc_settings(settings: RateControl) -> ObsData {
+fn nvidia_h264_settings(settings: RateControl) -> ObsData {
     let mut data = ObsData::new();
 
     data.set_int("bf", 2);
@@ -110,7 +96,64 @@ fn nvidia_nvenc_settings(settings: RateControl) -> ObsData {
     data
 }
 
-fn intel_quicksync_settings(settings: RateControl) -> ObsData {
+fn nvidia_av1_settings(settings: RateControl) -> ObsData {
+    let mut data = ObsData::new();
+
+    data.set_string("rate_control", "CQP");
+    data.set_string("profile", "main");
+
+    match settings {
+        RateControl::CBR(cbr) => {
+            data.set_string("rate_control", "CBR");
+            data.set_int("bitrate", cbr);
+        }
+        RateControl::VBR(vbr) => {
+            data.set_string("rate_control", "VBR");
+            data.set_int("bitrate", vbr);
+            data.set_int("max_bitrate", vbr + vbr / 2);
+        }
+        RateControl::CQP(cqp) => {
+            data.set_string("rate_control", "CQP");
+            data.set_int("cqp", cqp);
+            data.set_int("bitrate", 40000);
+            data.set_int("max_bitrate", 60000);
+        }
+        _ => {}
+    };
+    data
+}
+
+fn amd_amf_settings(rate_control: RateControl) -> ObsData {
+    let mut data = ObsData::new();
+
+    // Picture Control Properties
+    data.set_int("bf", 1);
+    data.set_int("keyint_sec", 2);
+    data.set_string("ffmpeg_opts", "MaxNumRefFrames=4 BReferenceEnable=1 BPicturesPattern=1 MaxConsecutiveBPictures=1 HighMotionQualityBoostEnable=1");
+
+    data.set_string("preset", "quality");
+    data.set_string("profile", "high");
+
+    match rate_control {
+        RateControl::CBR(cbr) => {
+            data.set_string("rate_control", "CBR");
+            data.set_int("bitrate", cbr);
+        }
+        RateControl::VBR(vbr) => {
+            data.set_string("rate_control", "VBR");
+            data.set_int("bitrate", vbr);
+        }
+        RateControl::CQP(cqp) | RateControl::ICQ(cqp) => {
+            let cqp = cqp.clamp(0, 51);
+            data.set_string("rate_control", "CQP");
+            data.set_int("cqp", cqp);
+        }
+        RateControl::CRF(_) => { /* invalid -> do nothing */ }
+    };
+    data
+}
+
+fn intel_quicksync_h264_settings(settings: RateControl) -> ObsData {
     let mut data = ObsData::new();
 
     data.set_string("profile", "high");
@@ -138,6 +181,24 @@ fn intel_quicksync_settings(settings: RateControl) -> ObsData {
             data.set_string("rate_control", "ICQ");
             data.set_int("icq_quality", icq);
         }
+    };
+    data
+}
+
+fn intel_quicksync_av1_settings(settings: RateControl) -> ObsData {
+    let mut data = ObsData::new();
+
+    data.set_string("profile", "high");
+
+    match settings {
+        RateControl::CQP(cqp) | RateControl::CRF(cqp) => {
+            let cqp = cqp.clamp(0, 51);
+            data.set_string("rate_control", "CQP");
+            data.set_int("qpi", cqp);
+            data.set_int("qpp", cqp);
+            data.set_int("qpb", cqp);
+        }
+        _ => {}
     };
     data
 }
